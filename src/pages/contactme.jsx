3606d49e6f157cb21ce1ecb98b31aab2a2063318
Cooljackup <cooljackup139@gmail.com>
2025-09-28 18:07:30 -0400

    Contact Me page finalized.
    Finished creating the contactme.jsx page, has a functioning page that can collect a name, email, and message and stores it. Doesn't actually send it anywhere, functionality not required. It is also stylized. Wrapping up project, doing final touches and bug fixes.

--- src/pages/contactme.jsx
+++ src/pages/contactme.jsx
@@ -1,7 +1,22 @@
 /// <!-- Name: Nicholas Bonneville - Student Number: 301194178 - September 28th, 2025 -->
 // Contact Me page. Allows the user to send me a message. Currently only captures the information and saves it.
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ContactMe() {
+    const [formData, setFormData] = useState({ name: "", email: "", message: "" });
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Form Submitted: ", formData);
+        navigate("/");
+    };
+
     return (
         <div>
             <div class="pages-div">
@@ -13,7 +28,12 @@ export default function ContactMe() {
             
             <div class="pages-div">
                 <div class="pages-text">
-
+                    <form className="contactme-form" onSubmit={handleSubmit}>
+                        <input placeholder="Your Name" name="name" required value={formData.name} onChange={handleChange}/>
+                        <input type="email" placeholder="Your Email" name="email" required value={formData.email} onChange={handleChange}/>
+                        <textarea placeholder="Your Message" name="message" required value={formData.message} onChange={handleChange}/>
+                        <button className="contactme-button">Send Message</button>
+                    </form>
                 </div>
             </div>
         </div>
